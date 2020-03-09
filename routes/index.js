e7b4fcb4e60f1e4818a2ded1ffbf1fc47042bff4
Angel <angelhodar76@gmail.com>
2020-03-09 16:58:14 +0100

    Added pug but not rendering data

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+// Importar io para el post
+
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.post('/', (req, res) => {
+  io.emit('exercise', req.body.exercise)
 })
 
 module.exports = router
