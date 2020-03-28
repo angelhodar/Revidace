c2f9ea8c05921bac3c34b6e6b73e3f9f04e3b103
Angel <angelhodar76@gmail.com>
2020-03-28 14:05:14 +0100

    Automated mongo database selection in db.js

--- db.js
+++ db.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_LOCAL, {
+var MONGO_URI = process.env.MONGO_LOCAL ? process.env.MONGO_LOCAL : process.env.MONGO_CLUSTER;
+
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
