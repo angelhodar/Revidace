cf14d863b607fd6d239963ba8544a54c28fe4198
Raul Sanchez <raulsf66@gmail.com>
2020-03-29 18:34:11 +0200

    #3: Register and module endpoints finished

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,12 +1,35 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 const router = express.Router()
+const User = require('../db/models/user')
 
 router.get('/', (req, res) => {
   res.render('login')
 })
 
 router.post('/', (req, res) => {
-  //TODO: Check DB and authenticate
+  var email = req.body.email;
+  var candidate_pass = req.body.password;
+  var query = User.findOne({ 'email': email })
+  var sessionData;
+
+  query.exec((err, user) => {
+    console
+    bcrypt.compare(candidate_pass, user.password, (err, result) => {
+      if (result) {
+        sessionData = req.session;
+        sessionData.user = {
+          "name": user.username
+        }
+        res.redirect('/dashboard');
+      } else {
+        res.redirect('/');
+      }
+    });
+
+  });
+
+
 })
 
 module.exports = router
--- src/routes/register.js
+++ src/routes/register.js
@@ -1,12 +1,35 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 const router = express.Router()
+const User = require('../db/models/user')
 
 router.get('/', (req, res) => {
   res.render('register')
 })
 
 router.post('/', (req, res) => {
-  //TODO: Check, encrypt and save to DB
+  var username = req.body.username;
+  var email = req.body.email;
+  var pass = req.body.password;
+
+  bcrypt.hash(pass, 10, (err, hash) => {
+    pass = hash
+
+    var new_user = new User({
+      "email": email,
+      "password": pass,
+      "username": username,
+    });
+
+    new_user.save((err) => {
+      if (err)
+        console.log('Error saving user: ' + err);
+      else
+        console.log('User saved');
+    });
+  })
+
+  res.redirect('/');
 })
 
 module.exports = router
